3f27c5cbc611a6c80bf61c047d40320bb1337a74
ms5788 <ms5788@drexel.edu>
2026-08-31 03:12:17 -0400

    Remove trailing commas from coins and holdings table definitions

--- server/src/db/schemas/holdings.sql
+++ server/src/db/schemas/holdings.sql
@@ -4,6 +4,6 @@ create table holdings (
     id uuid primary key default gen_random_uuid(),  -- temp will have login feature so will be tied to a real user id later
     symbol text not null,   -- the coin the user owns, ie: bitcoin
     quantity numeric(28,8) not null,    -- the amount the user owns
-    avg_cost_basis numeric(20,8) not null,   -- cost of the coin when first purchased
+    avg_cost_basis numeric(20,8) not null   -- cost of the coin when first purchased
 )
 
